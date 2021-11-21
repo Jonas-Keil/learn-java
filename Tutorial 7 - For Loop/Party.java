0f506c58fa3a2558dfc42afffcedb54d2a1b1a84
Jonas Keil <89358061+Jonas-Keil@users.noreply.github.com>
2021-11-21 19:07:25 +0100

    Create Party.java

---
+++ Tutorial 7 - For Loop/Party.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Party {
+    public static void main(String[] args) {
+        int guests;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Anzahl der angemeldeten Gäste: ");
+        guests = scan.nextInt();
+
+        while (true) {
+            System.out.println("+X Gäste anmelden, 0 Programm beenden, -X Gäste abmelden");
+            int value = scan.nextInt();
+            if (value == 0) {
+                break;
+            }
+            if (value > 0) {
+                for (int i = 0; i < value; i++) {
+                    System.out.println("Gast angemeldet");
+                    guests++;
+                }
+            }
+            if (value < 0) {
+                for (int i = 0; i > value; i--) {
+                    System.out.println("Gast abgemeldet");
+                    guests--;
+                }
+            }
+            System.out.println(guests + " Gäste angemeldet");
+        }
+
+    }
+}
